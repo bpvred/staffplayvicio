81ab6641ab78bc5b5acf1e4e83348f745cfd1ac9
bpvred <ddusardom@gmail.com>
2026-07-24 13:34:03 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-// Configuração do formulário BPV.
-// ATENÇÃO: em hospedagem pública, este webhook poderá ser visto no código-fonte.
+// Configuração do formulário BPV
+
 window.BPV_CONFIG = {
-  DISCORD_WEBHOOK_URL: "https://discord.com/api/webhooks/1530206837746565230/XDWBq5oQ0l8z8o4pymT8v0CjbyJaASJFopmjg_HQAtb8p9K1VyaH4olz9WWCBt1U0-MD",
-  SERVER_NAME: "Play Vício Roleplay VRed"
+  API_URL: "https://bpv-staff-bot.onrender.com/candidatura",
+  SERVER_NAME: "Play Vício Roleplay vRed"
 };
